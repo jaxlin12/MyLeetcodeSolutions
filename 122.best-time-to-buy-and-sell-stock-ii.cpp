8674c24c0acdc8f555b02c55ad2c595fe4d9c862
JaxLam <jaxkinglam@gmail.com>
2019-08-30 18:29:28 -0700

    p122

--- 122.best-time-to-buy-and-sell-stock-ii.cpp
+++ 122.best-time-to-buy-and-sell-stock-ii.cpp
@@ -4,32 +4,14 @@
  * [122] Best Time to Buy and Sell Stock II
  */
 class Solution {
-public:
+    public:
     int maxProfit(vector<int>& prices) {
-        int size = prices.size();
-        if(size < 2) return 0;
-        int vallyIndex = 0;
-        int maxProfit = 0;
-        int i = 1;
-        while(prices[i] <= prices[vallyIndex] && i < size-1) {
-            vallyIndex = i;
-            ++i;
+        int maxprofit = 0;
+        for (int i = 1; i < prices.size(); i++) {
+            if (prices[i] > prices[i - 1])
+                maxprofit += prices[i] - prices[i - 1];
         }
-        for(i -= 1;i < size-1;++i) {
-            if(prices[i] > prices[vallyIndex] && prices[i] >= prices[i+1]) {
-                maxProfit = maxProfit + prices[i] - prices[vallyIndex];
-                vallyIndex = i;
-                ++i;
-                while(prices[i] <= prices[vallyIndex] && i < size-1) {
-                    vallyIndex = i;
-                    ++i;
-                }
-                --i;
-            }
-        }
-        if(vallyIndex < size - 1 && prices[size - 1] > prices[vallyIndex])
-            maxProfit += prices[size - 1] - prices[vallyIndex];
-        return maxProfit;
+        return maxprofit;
     }
 };
 
