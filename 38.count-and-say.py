347e02bd7ca3a25fa8e5bb2d9f9741c60c71a51a
jaxlin12 <jaxkinglam@gmail.com>
2020-08-25 18:54:15 -0700

    p.38

--- 38.count-and-say.py
+++ 38.count-and-say.py
@@ -7,25 +7,21 @@
 # @lc code=start
 class Solution:
     def countAndSay(self, n: int) -> str:
-        sequence = ""
-        for i in range(1, n+1):
-            if i == 1:
-                sequence = "1"
-            else:
-                new_sequence = ""
-                count = 0
-                say = sequence[0]
-                for char in sequence:
-                    if char == say:
-                        count = count + 1
-                    else:
-                        new_sequence = new_sequence + str(count) + say
-                        count = 1
-                        say = char
-                if count != 0:
+        sequence = "1"
+        for i in range(2, n+1):
+            new_sequence = ""
+            count = 0
+            say = sequence[0]
+            for char in sequence:
+                if char == say:
+                    count = count + 1
+                else:
                     new_sequence = new_sequence + str(count) + say
-                    
-                sequence = new_sequence
+                    count = 1
+                    say = char
+            if count != 0:
+                new_sequence = new_sequence + str(count) + say        
+            sequence = new_sequence
         return sequence
                 
 # @lc code=end
